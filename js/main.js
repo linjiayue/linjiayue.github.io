0cc1ac06b67a4320655b31ae290a876fd7d65ac6
林家跃 <linjiayue@linjiayuedeMacBook-Pro.local>
2017-04-20 18:53:35 +0800

    增加侧边栏失去焦点方法

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-//避免DOM元素未加载导致监听无法正常工作
-$("body").delegate("#slibarIcon", "click", function () {  
+
+$("#slibarIcon").click(function(){
 	if($("#sidebar").hasClass("sidebarMoveToRight")){
 		//根据内容高度设置侧边栏高度
-		var clientHeight = document.documentElement.clientHeight;
-		var bodyHeight = $("body").height();
-		if(clientHeight > bodyHeight){
-			$("#sidebar").height(clientHeight);
-		}else{
-			$("#sidebar").height(bodyHeight);
-		}
+		// var clientHeight = document.documentElement.clientHeight;
+		// var bodyHeight = $("body").height();
+		// if(clientHeight > bodyHeight){
+		// 	$("#sidebar").height(clientHeight);
+		// }else{
+		// 	$("#sidebar").height(bodyHeight);
+		// }
 
 		$("#content").removeClass("contentMoveToRight");
 		$("#sidebar").removeClass("sidebarMoveToRight"); 
@@ -20,6 +20,8 @@ $("body").delegate("#slibarIcon", "click", function () {
 
 		$("#slibarIcon").removeClass("glyphicon glyphicon-menu-hamburger"); 
 		$("#slibarIcon").addClass("glyphicon glyphicon-remove"); 
+		//获得焦点
+		$("#sidebar").focus();
 	}else{
 		$("#content").removeClass("contentMoveToLeft");
 		$("#sidebar").removeClass("sidebarMoveToLeft"); 
@@ -30,5 +32,15 @@ $("body").delegate("#slibarIcon", "click", function () {
 		$("#slibarIcon").addClass("glyphicon glyphicon-menu-hamburger"); 
 		// $("#sidebar").hide(); 
 	}
-	
-})
+});
+
+$("#sidebar").blur(function(){
+	$("#content").removeClass("contentMoveToLeft");
+	$("#sidebar").removeClass("sidebarMoveToLeft"); 
+	$("#content").addClass("contentMoveToRight");
+	$("#sidebar").addClass("sidebarMoveToRight"); 
+
+	$("#slibarIcon").removeClass("glyphicon glyphicon-remove"); 
+	$("#slibarIcon").addClass("glyphicon glyphicon-menu-hamburger"); 
+});
+
