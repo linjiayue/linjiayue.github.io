34c0c2d11040fb9e79fd8d5559649793ec2cc1e4
林家跃 <linjiayue@linjiayuedeMacBook-Pro.local>
2017-04-20 16:36:59 +0800

    解决侧边栏兼容性问题

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 //避免DOM元素未加载导致监听无法正常工作
 $("body").delegate("#slibarIcon", "click", function () {  
 	if($("#sidebar").hasClass("sidebarMoveToRight")){
+		//根据内容高度设置侧边栏高度
+		var clientHeight = document.documentElement.clientHeight;
+		var bodyHeight = $("body").height();
+		if(clientHeight > bodyHeight){
+			$("#sidebar").height(clientHeight);
+		}else{
+			$("#sidebar").height(bodyHeight);
+		}
+
 		$("#content").removeClass("contentMoveToRight");
 		$("#sidebar").removeClass("sidebarMoveToRight"); 
 		$("#content").addClass("contentMoveToLeft");
-		$("#sidebar").show(); 
+		
+		
 		$("#sidebar").addClass("sidebarMoveToLeft");
+		$("#sidebar").show(); 
 
 		$("#slibarIcon").removeClass("glyphicon glyphicon-menu-hamburger"); 
 		$("#slibarIcon").addClass("glyphicon glyphicon-remove"); 
@@ -17,7 +28,7 @@ $("body").delegate("#slibarIcon", "click", function () {
 
 		$("#slibarIcon").removeClass("glyphicon glyphicon-remove"); 
 		$("#slibarIcon").addClass("glyphicon glyphicon-menu-hamburger"); 
-		$("#sidebar").hide(); 
+		// $("#sidebar").hide(); 
 	}
 	
 })
